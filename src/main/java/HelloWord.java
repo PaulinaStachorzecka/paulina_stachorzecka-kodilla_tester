8562e5291ad837ab72dccb07e4d353a2331a63ec
Paulina Stachorzecka <stachorzeckapaulina@gmail.com>
2025-06-17 15:19:28 +0200

    Modify code

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord {
+    public static void mail (String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("Paulina");
+        System.out.println(29);
+        System.out.println('S');
+    }
+}
