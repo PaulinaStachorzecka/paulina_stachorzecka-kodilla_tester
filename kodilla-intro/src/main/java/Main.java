9949c8705367390dc0dee1e2262b70d77b00557c
Paulina Stachorzecka <stachorzeckapaulina@gmail.com>
2025-07-17 22:23:50 +0000

    Pierwszy commit - dodanie kodu Kodilla Intro

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Paulina";
+        int myAge = 29;
+        double height = 1.64;
+        char carClass = 'B';
+        boolean isGoodProgrammer = true;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+
+public class MainExtended {
+    public static void main(String[] args) {
+        String favoriteBook = "Harry Potter";
+        String author = "J.K. Rowling";
+        String favoriteVolume = "Książę Półkrwi";
+        int numberOfPages = 704;
+        double date = 07.2005;
+        boolean theBestBook = true;
+
+        System.out.println(favoriteBook);
+        System.out.println(author);
+        System.out.println(favoriteVolume);
+        System.out.println(numberOfPages);
+        System.out.println(date);
+        System.out.println(theBestBook);
+    }
+}
