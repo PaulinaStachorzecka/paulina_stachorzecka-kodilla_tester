ff661c6e96c99dd6ca1a907bceab32ef29e19c58
Paulina Stachorzecka <stachorzeckapaulina@gmail.com>
2025-07-20 11:18:01 +0000

    Zadanie - rok przestępny

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2025;
+
+        if ( year % 400 == 0) {
+            System.out.println(year + " Jest rokiem przestępnym.");
+        } else if ( year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " Jest rokiem przestępnym.");
+        } else {
+            System.out.println(year + " Nie jest rokiem przestępnym.");
+        }
+    }
 }
